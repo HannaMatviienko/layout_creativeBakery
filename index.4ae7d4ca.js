41d17d6f5190f6ccfbfb3bfaef045c473b7538af
Hanna <hmatviienko@gmail.com>
2024-05-23 13:45:59 +0300

    make build

---
+++ index.4ae7d4ca.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('a[href="#nav"]'),o=document.querySelector(".header__cover--top--nav--top--close"),c=document.body;e.addEventListener("click",()=>{console.log("Open"),c.classList.add("no-scroll")}),o.addEventListener("click",()=>{console.log("Close"),c.classList.remove("no-scroll")});
+//# sourceMappingURL=index.4ae7d4ca.js.map
